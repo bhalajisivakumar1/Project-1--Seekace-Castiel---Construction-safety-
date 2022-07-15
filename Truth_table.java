9d32080aa95c27835765a41157bcf79c14933388
bhalajisivakumar1 <97597567+bhalajisivakumar1@users.noreply.github.com>
2022-07-15 09:12:10 +0530

    Add files via upload

---
+++ Truth_table.java
@@ -0,0 +1,20 @@
+package test;
+
+public class newTest {
+	 
+
+	    private static void printTruthTable(int n) {
+	        int rows = (int) Math.pow(2,n);
+
+	        for (int i=0; i<rows; i++) {
+	            for (int j=n-1; j>=0; j--) {
+	                System.out.print((i/(int) Math.pow(2, j))%2 + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
+	    public static void main(String[] args) {
+	        printTruthTable(9); //enter any natural int
+	    }
+	}
+
